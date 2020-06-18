71ae118f57e12faa0750031192924077a55aa0cd
Sandro Hallado <sandrohallado@yahoo.com>
2020-06-17 23:13:20 -0700

    Setup database configuration

---
+++ models/index.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const {
+  database,
+  username,
+  password,
+  host,
+  dialect
+} = require('../config/db');
+
+const sequelize = new Sequelize(database, username, password, {
+  host: 'localhost',
+  log: false,
+  define: {
+    underscored: true
+  },
+  dialect: 'mysql'
+});
+
+sequelize.authenticate()
+  .then(() => console.log('Successfully connected to the database'))
+  .catch((err) => console.log('Unable to connect to the database', err));
