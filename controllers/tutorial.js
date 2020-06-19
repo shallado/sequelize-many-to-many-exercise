6f86cdf2369a54f21dc732932fe1602c815980b6
Sandro Hallado <sandrohallado@yahoo.com>
2020-06-18 19:22:27 -0700

    Setup method to find all tutorials

--- controllers/tutorial.js
+++ controllers/tutorial.js
@@ -1,4 +1,4 @@
-const { Tutorial } = require('../models');
+const { Tutorial, Tag } = require('../models');
 
 exports.createTutorial = (tutorialInfo) => {
   return Tutorial.create(tutorialInfo)
@@ -7,4 +7,21 @@ exports.createTutorial = (tutorialInfo) => {
       data: tutorial.get()
     }))
     .catch(() => console.log('Error while creating a tutorial', err));
+};
+
+exports.findAllTutorials = () => {
+  return Tutorial.findAll({ 
+    include: {
+      model: Tag,
+      as: 'tags'
+    } 
+  })
+    .then((tutorials) => {
+      if (tutorials.length === 0) {
+        console.log('Unable to find any tutorials');
+      }
+
+      return tutorials.map((tutorial) => tutorial.get());
+    })
+    .catch((err) => console.log('Error finding tutorials'));
 };
